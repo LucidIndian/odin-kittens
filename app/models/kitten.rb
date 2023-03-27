dc04af8dd33dad89020a922f181c7f8eeddcb0c4
Tygh Walters <tyghwalters@gmail.com>
2023-03-26 21:47:16 -0700

    Custom Validations and Error Messages
    Also fixed the broken delete button for each kitten on Kittens#Index

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,2 +1,8 @@
 class Kitten < ApplicationRecord
+
+  validates :name, presence: { message: "All kittens have names, silly!" }
+  validates :cuteness, presence: { message: "0 is ugly, 10 is CUTE AF" }
+  validates :age, presence: { message: "How young is your cat?!" }
+  validates :softness, presence: { message: "0 is rough, 10 is smooth" }
+  validates :cuteness, :softness, numericality: { less_than_or_equal_to: 10, greater_than_or_equal_to: 0, message: "%{value} seems wrong, Please use numbers less than ten!" }
 end
